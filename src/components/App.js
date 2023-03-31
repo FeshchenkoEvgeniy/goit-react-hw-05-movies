ef5c91733a0602dfd12ee4dce417c4d8735993e9
Evgeniy <feshchenko.evgeniy03@gmail.com>
2023-03-31 19:54:45 +0300

    updATE

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,16 @@ import Movies from 'pages/Movies/Movies';
 import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home/Home';
 import { Layout } from './Layout/Layout';
+import MovieDetails from '../pages/MovieDetails/MovieDetails';
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:id" element={<MovieDetails />} />
+          <Route path="*" element={<Home />} />
         </Route>
       </Routes>
     </div>
--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,9 +1,16 @@
+import { Link, useLocation } from "react-router-dom";
+
+
 export const MovieList = ({ movies }) => {
+
+  const location = useLocation();
+
   return (
     <div>
       <ul>
         {movies.map(({ id, title, poster_path }) => (
           <li key={id}>
+            <Link to={`${id}`}>
             <img
               src={
                 poster_path
@@ -14,6 +21,7 @@ export const MovieList = ({ movies }) => {
               width="250px"
             />
             <p>{title}</p>
+            </Link>
           </li>
         ))}
       </ul>
